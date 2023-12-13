ff1c48a7ea7c7f24d0a24fbc886e27efde0240ae
cecile.talec13 <cecile.talec13@gmail.com>
2023-12-13 16:42:54 +0100

    Q 0.1 finie

--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -13,6 +13,28 @@ public class CoordinateTest {
 		Coordinate c = new Coordinate(3.0,4.0);
 		Assert.assertEquals(3.0, c.getX(), EPSILON);
 		Assert.assertEquals(4.0, c.getY(), EPSILON);
+		Assert.assertFalse(c.isEmpty());
+		Assert.assertEquals("[3.0,4.0]", c.toString());
+	}
+	
+	@Test
+	public void testConstructorEmpty(){
+		// TODO
+		Coordinate c = new Coordinate();
+		Assert.assertEquals(Double.NaN, c.getX(), EPSILON);
+		Assert.assertEquals(Double.NaN, c.getY(), EPSILON);
+		Assert.assertTrue(c.isEmpty());
+		Assert.assertEquals("[NaN,NaN]", c.toString());
+	}
+	
+	@Test
+	public void testConstructorHalfEmpty(){
+		// TODO
+		Coordinate c = new Coordinate(3.5, Double.NaN);
+		Assert.assertEquals(3.5, c.getX(), EPSILON);
+		Assert.assertEquals(Double.NaN, c.getY(), EPSILON);
+		Assert.assertTrue(c.isEmpty());
+		Assert.assertEquals("[3.5,NaN]", c.toString());
 	}
 
 }
